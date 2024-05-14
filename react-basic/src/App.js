f50a9c9bd64173426f6bbf159617881c3ca382f7
s-chance <entropy.tree@outlook.com>
2024-05-14 13:25:24 +0800

    feat(*): son component communicate with father component

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,28 @@
-function App() {
+import { useState } from 'react'
+
+function Son({ onGetMsg }) {
+  const sonMsg = 'this is son msg'
   return (
     <div>
+      this is Son
+      <button onClick={() => onGetMsg(sonMsg)}>sendMsg</button>
+    </div>
+  )
+}
 
+function App() {
+
+  const [msg, setMsg] = useState('')
+  
+  const getMsg = (msg) => {
+    console.log(msg)
+    setMsg(msg)
+  }
+
+  return (
+    <div>
+      this is App, {msg}
+      <Son onGetMsg={getMsg} />
     </div>
   )
 }
