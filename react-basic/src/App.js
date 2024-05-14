170fd12aec9ed931565d9c6dd48c50ae49cf4975
s-chance <entropy.tree@outlook.com>
2024-05-14 17:49:56 +0800

    feat(*): clear the effect from 'useEffect'

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function Son() {
+  //  渲染时开启一个定时器
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log('定时器执行中...');
+    }, 1000)
+    return () => {
+      // 清除副作用(组件卸载时)
+      clearInterval(timer)
+    }
+  }, [])
+  return <div>this is son</div>
+}
+
 function App() {
+  const [show, setShow] = useState(true)
   return (
     <div>
-
+      {show && <Son />}
+      <button onClick={() => setShow(false)}>卸载Son组件</button>
     </div>
   )
 }
