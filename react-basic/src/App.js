fd22c4d795fc5184ee61cd286be7a316aecfc1ec
s-chance <entropy.tree@outlook.com>
2024-05-14 14:59:15 +0800

    feat(*): 'useEffect' demo code

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react'
+
+const URL = 'https://geek.itheima.net/v1_0/channels'
+
 function App() {
+  // 创建状态数据
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    // 获取数据
+    async function getList() {
+      const res = await fetch(URL)
+      const jsonRes = await res.json()
+      console.log(jsonRes)
+      setList(jsonRes.data.channels)
+    }
+    getList()
+  }, [])
   return (
     <div>
-
+      this is app
+      <ul>
+        {list.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   )
 }
