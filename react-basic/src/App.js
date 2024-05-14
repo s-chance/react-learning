d3df95f661294e5b8501ed1f8d66a2011f4dc4db
s-chance <entropy.tree@outlook.com>
2024-05-14 13:58:58 +0800

    feat(*): implement brother components communication

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,39 @@
-function App() {
+// 通过子传父 A -> App
+// 通过父传子 App -> B
+import { useState } from 'react'
+
+function A({ onGetAName }) {
+  const name = 'this is A name'
   return (
     <div>
+      this is A component
+      <button onClick={() => onGetAName(name)}>send</button>
+    </div>
+  )
+}
 
+function B({ name }) {
+  return (
+    <div>
+      this is B component, {name}
+    </div>
+  )
+}
+
+
+function App() {
+  const [name, setName] = useState('')
+
+  const getAName = (name) => {
+    console.log(name);
+    setName(name)
+  }
+
+  return (
+    <div>
+      this is App component
+      <A onGetAName={getAName} />
+      <B name={name} />
     </div>
   )
 }
