e398d17b4fa024d0187ab646a56d7a88dfecb462
s-chance <entropy.tree@outlook.com>
2024-05-14 21:25:38 +0800

    feat(*): custom hooks funciton

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
+
+// 自定义hook
+function useToggle() {
+  // 可复用的逻辑
+  const [value, setValue] = useState(true);
+
+  const toggle = () => setValue(!value);
+
+  // 哪些状态和回调函数需要在外部使用，就返回哪些
+  return { value, toggle };
+}
+
 function App() {
+
+  const { value, toggle } = useToggle();
+
   return (
     <div>
-
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
     </div>
   )
 }
