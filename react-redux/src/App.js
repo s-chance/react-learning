35273e89808bfda538941829a2c3e2b0fde8bda3
s-chance <entropy.tree@outlook.com>
2024-05-16 23:24:16 +0800

    feat(*): react-redux demo

---
+++ react-redux/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+// 导入 actionCreator
+import { increment, decrement, addToNum } from "./store/modules/counterStore";
+import { fetchChannelList } from "./store/modules/channelStore";
+function App() {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(state => state.channel)
+  const dispatch = useDispatch()
+  // 使用useEffect触发异步请求
+  useEffect(() => {
+    dispatch(fetchChannelList())
+  }, [dispatch])
+  return (
+    <div className="App">
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {count}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>add To 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add To 20</button>
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
