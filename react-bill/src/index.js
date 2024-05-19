71f4d9b690e47f5aba13b041af51fd8f30654ba6
s-chance <entropy.tree@outlook.com>
2024-05-19 17:34:39 +0800

    feat(*): path alias test

--- react-bill/src/index.js
+++ react-bill/src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import sum from '@/test';
+
+const total = sum(1, 3)
+console.log(total);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <App />
 );
