d0582b3ff443d0ea6b923510257def43602b6ab5
s-chance <entropy.tree@outlook.com>
2024-05-24 15:00:21 +0800

    feat(*): login page

--- react-vite-project/src/pages/Login/index.tsx
+++ react-vite-project/src/pages/Login/index.tsx
@@ -1,8 +1,27 @@
+import { Avatar, Button, Card, Form, Input } from "antd";
+import "./index.scss";
+import avatar from "@/assets/avatar.avif";
+
 const Login = () => {
   return (
-    <>
-      <div>this is login</div>
-    </>
+    <div className="login">
+      <Card className="login-container">
+        <Avatar size={64} src={avatar} className="login-avatar" />
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" size="large">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
