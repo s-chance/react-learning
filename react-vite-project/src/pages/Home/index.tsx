a33989d44032431b9d6964adb03d29a32950aece
s-chance <entropy.tree@outlook.com>
2024-05-26 01:26:39 +0800

    feat(*): echarts initial

--- react-vite-project/src/pages/Home/index.tsx
+++ react-vite-project/src/pages/Home/index.tsx
@@ -1,5 +1,44 @@
+import * as echarts from "echarts/core";
+import { GridComponent, GridComponentOption } from "echarts/components";
+import { BarChart, BarSeriesOption } from "echarts/charts";
+import { CanvasRenderer } from "echarts/renderers";
+import { useEffect, useRef } from "react";
+
+echarts.use([GridComponent, BarChart, CanvasRenderer]);
+
+type EChartsOption = echarts.ComposeOption<
+  GridComponentOption | BarSeriesOption
+>;
+
+const option: EChartsOption = {
+  xAxis: {
+    type: "category",
+    data: ["Vue", "React", "Angular"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [10, 40, 70],
+      type: "bar",
+    },
+  ],
+};
+
 const Home = () => {
-  return <div>this is home</div>;
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+    option && myChart.setOption(option);
+  });
+  return (
+    <div>
+      <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+    </div>
+  );
 };
 
 export default Home;
