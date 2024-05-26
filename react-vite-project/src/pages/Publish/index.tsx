d80a6c76550a406c6a2b7d65d62b0774f92c89c1
s-chance <entropy.tree@outlook.com>
2024-05-26 12:13:50 +0800

    feat(*): publish page

--- react-vite-project/src/pages/Publish/index.tsx
+++ react-vite-project/src/pages/Publish/index.tsx
@@ -1,5 +1,54 @@
+import { Breadcrumb, Button, Card, Form, Input, Select, Space } from "antd";
+import { Link } from "react-router-dom";
+import "./index.scss";
+
 const Publish = () => {
-  return <div>this is publish</div>;
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb
+            items={[{ title: <Link to="/">首页</Link> }, { title: "发布文章" }]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: "请输入标题" }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: "请选择频道" }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              <Select.Option value={1}>前端</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: "请输入内容" }]}
+          ></Form.Item>
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Publish;
