7db098d3cdc7d3ed20f67756e752268c85d456f2
s-chance <entropy.tree@outlook.com>
2024-06-01 02:06:14 +0800

    build(*): using rollup-plugin-visualizer to analyze dist dir

--- react-vite-project/vite.config.ts
+++ react-vite-project/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [react()],
+  plugins: [react(), visualizer() as PluginOption],
 });
