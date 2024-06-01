f0f9a1d67ce80dc977bcecd306b1470faf717daf
s-chance <entropy.tree@outlook.com>
2024-06-01 22:59:35 +0800

    feat(*): useCallback

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,21 @@
+import { memo, useCallback, useState } from "react";
+
+type onChangeType = { onChange: (value: string) => void };
+
+const Input = memo(function Input({ onChange }: onChangeType) {
+  console.log("Input render");
+  return <input type="text" onChange={(e) => onChange(e.target.value)} />;
+});
+
 function App() {
-  return <div></div>;
+  const changeHandler = useCallback((value: string) => console.log(value), []);
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <Input onChange={changeHandler} />
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </div>
+  );
 }
 
 export default App;
