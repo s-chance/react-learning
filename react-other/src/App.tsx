1352bfeee5a9f4e359749b80883cee73f150716b
s-chance <entropy.tree@outlook.com>
2024-06-01 21:26:18 +0800

    feat(*): useReducer

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,33 @@
+import { useReducer } from "react";
+
+function reducer(state: number, action: { type: string; payload?: number }) {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "SET":
+      if (!action.payload) {
+        throw new Error("payload is required");
+      }
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
 function App() {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, 0);
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "INC" })}>+</button>
+      {state}
+      <button onClick={() => dispatch({ type: "DEC" })}>-</button>
+      <button onClick={() => dispatch({ type: "SET", payload: 100 })}>
+        update
+      </button>
+    </div>
+  );
 }
 
 export default App;
