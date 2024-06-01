7613a6d27d94255401f9d30025130959c8b019ef
s-chance <entropy.tree@outlook.com>
2024-06-01 22:46:35 +0800

    feat(*): react.memo props compare mechanism

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,6 +1,6 @@
-import { memo, useState } from "react";
+import { memo, useMemo, useState } from "react";
 
-const MemoSon = memo(function Son(props: { props: number }) {
+const MemoSon = memo(function Son(props: { props: number | number[] }) {
   console.log("son component,", props.props);
   return <div>this is son</div>;
 });
@@ -10,12 +10,17 @@ function App() {
 
   const [props, setProps] = useState(0);
 
+  const list = useMemo(() => {
+    return [1, 2, 3];
+  }, []);
+
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>+{count}</button>
       <button onClick={() => setProps(props + 1)}>change props: {props}</button>
       {/* <Son /> */}
-      <MemoSon props={props} />
+      <MemoSon props={100} />
+      <MemoSon props={list} />
     </div>
   );
 }
