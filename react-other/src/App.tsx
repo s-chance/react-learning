5fb92761926ebaa49d9a99e1527660029143b846
s-chance <entropy.tree@outlook.com>
2024-06-02 01:50:05 +0800

    feat(*): async request

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,20 +1,41 @@
+import { useEffect } from "react";
 import { create } from "zustand";
 
 type Store = {
   count: number;
   inc: () => void;
+  data: {
+    id: string;
+    name: string;
+  }[];
+  fetchData: () => void;
 };
+
+const URL = "https://example.com";
 
 const useStore = create<Store>()((set) => ({
   count: 1,
   inc: () => set((state) => ({ count: state.count + 1 })),
+  data: [],
+  fetchData: async () => {
+    const res = await fetch(URL);
+    const jsonRes = await res.json();
+    console.log(jsonRes);
+    set({ data: jsonRes.data.channels });
+  },
 }));
 
 function Counter() {
-  const { count, inc } = useStore();
+  const { count, inc, data, fetchData } = useStore();
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <div>
       <button onClick={inc}>{count}</button>
+      {data.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
     </div>
   );
 }
