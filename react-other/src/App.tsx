98044c5136738b72d765bd1d1db74785629813ff
s-chance <entropy.tree@outlook.com>
2024-06-02 00:31:26 +0800

    feat(*): class api

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,26 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+  clickHandler = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return <button onClick={this.clickHandler}>{this.state.count}</button>;
+  }
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
