5578687de14dc96398961ef775778a538af77161
s-chance <entropy.tree@outlook.com>
2024-06-02 00:40:53 +0800

    feat(*): class api lifecycle

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,30 @@
+import { Component, ReactNode, useState } from "react";
+
+class Son extends Component {
+  timer: NodeJS.Timeout | null = null;
+  componentDidMount(): void {
+    console.log("son componentDidMount");
+    this.timer = setInterval(() => {
+      console.log("timer running");
+    }, 1000);
+  }
+  componentWillUnmount(): void {
+    console.log("son componentWillUnmount");
+    clearInterval(this.timer!);
+  }
+  render(): ReactNode {
+    return <div>son</div>;
+  }
+}
+
 function App() {
-  return <div></div>;
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <button onClick={() => setShow(!show)}>toggle</button>
+      {show && <Son />}
+    </div>
+  );
 }
 
 export default App;
