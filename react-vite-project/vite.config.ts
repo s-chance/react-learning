044e6383dcb98c482bb6912d33eb0735db041688
s-chance <entropy.tree@outlook.com>
2024-06-01 15:54:55 +0800

    build(*): using cdn import manually without packageing  to dist

--- react-vite-project/vite.config.ts
+++ react-vite-project/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import externalGlobals from "rollup-plugin-external-globals";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,17 @@ export default defineConfig({
     },
   },
   plugins: [react(), visualizer() as PluginOption],
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom", "dayjs", "antd"],
+      plugins: [
+        externalGlobals({
+          react: "React",
+          "react-dom": "ReactDOM",
+          dayjs: "dayjs",
+          antd: "antd",
+        }),
+      ],
+    },
+  },
 });
