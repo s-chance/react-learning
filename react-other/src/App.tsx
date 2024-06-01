c2ad64250188a7501e67a2b0afd6b91a7c02674b
s-chance <entropy.tree@outlook.com>
2024-06-02 00:24:52 +0800

    feat(*): useImperativeHandle

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,30 @@
+import { Ref, forwardRef, useImperativeHandle, useRef } from "react";
+
+const Son = forwardRef((_, ref: Ref<object>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const focusHandler = () => {
+    inputRef.current?.focus();
+  };
+  useImperativeHandle(ref, () => {
+    return {
+      focusHandler,
+    };
+  });
+  return <input type="text" ref={inputRef} />;
+});
+
 function App() {
-  return <div></div>;
+  const sonRef = useRef<HTMLInputElement & { focusHandler: () => void }>(null);
+  const focusHandler = () => {
+    console.log(sonRef.current);
+    sonRef.current?.focusHandler();
+  };
+  return (
+    <div>
+      <Son ref={sonRef} />
+      <button onClick={focusHandler}>focus</button>
+    </div>
+  );
 }
 
 export default App;
