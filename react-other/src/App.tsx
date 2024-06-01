ff94d457f653910fce98c517daad589c9fce6db6
s-chance <entropy.tree@outlook.com>
2024-06-02 00:58:13 +0800

    feat(*): class api communication

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,50 @@
+import { Component, ReactNode } from "react";
+
+interface SonProps {
+  msg?: string;
+  onGetMsg?: (msg: string) => void;
+}
+
+class Son extends Component<SonProps> {
+  render(): ReactNode {
+    return (
+      <div>
+        <div>son,{this.props.msg}</div>
+        <button
+          onClick={() => {
+            this.props.onGetMsg?.("son message");
+          }}
+        >
+          send msg to parent
+        </button>
+      </div>
+    );
+  }
+}
+
+class Parent extends Component {
+  state = {
+    msg: "parent message",
+  };
+
+  getSonMsg = (sonMsg: string) => {
+    console.log(sonMsg);
+  };
+  render(): ReactNode {
+    return (
+      <div>
+        parent<Son msg={this.state.msg} onGetMsg={this.getSonMsg} />
+      </div>
+    );
+  }
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
 }
 
 export default App;
