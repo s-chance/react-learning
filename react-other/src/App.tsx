ac90863af04b6a67387ad32b5b0617bde34dd492
s-chance <entropy.tree@outlook.com>
2024-06-01 22:17:45 +0800

    feat(*): react.memo

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,23 @@
+import { memo, useState } from "react";
+
+const MemoSon = memo(function Son(props: { props: number }) {
+  console.log("son component,", props.props);
+  return <div>this is son</div>;
+});
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  const [props, setProps] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>+{count}</button>
+      <button onClick={() => setProps(props + 1)}>change props: {props}</button>
+      {/* <Son /> */}
+      <MemoSon props={props} />
+    </div>
+  );
 }
 
 export default App;
