fb3a0e8580e1c4d95b4c1bdc723a3f19295b1a83
s-chance <entropy.tree@outlook.com>
2024-06-01 17:00:43 +0800

    build(*): auto import cdn link in index.html by using vite-plugin-cdn-import

--- react-vite-project/vite.config.ts
+++ react-vite-project/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
-import externalGlobals from "rollup-plugin-external-globals";
+// import externalGlobals from "rollup-plugin-external-globals";
+import { Plugin as importToCDN } from "vite-plugin-cdn-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,18 +12,38 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [react(), visualizer() as PluginOption],
+  plugins: [
+    react(),
+    visualizer() as PluginOption,
+    importToCDN({
+      prodUrl: "https://cdnjs.cloudflare.com/ajax/libs/{name}/{version}/{path}",
+      modules: [
+        {
+          name: "react",
+          var: "React",
+          path: `umd/react.production.min.js`,
+        },
+        {
+          name: "react-dom",
+          var: "ReactDOM",
+          path: `umd/react-dom.production.min.js`,
+        },
+        {
+          name: "dayjs",
+          var: "dayjs",
+          path: `dayjs.min.js`,
+        },
+        {
+          name: "antd",
+          var: "antd",
+          path: `antd.min.js`,
+        },
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       external: ["react", "react-dom", "dayjs", "antd"],
-      plugins: [
-        externalGlobals({
-          react: "React",
-          "react-dom": "ReactDOM",
-          dayjs: "dayjs",
-          antd: "antd",
-        }),
-      ],
     },
   },
 });
