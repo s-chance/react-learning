cf7ad383d7666aa7d607c5900d79e861ccbd6045
s-chance <entropy.tree@outlook.com>
2024-06-02 01:19:09 +0800

    feat(*): zustand demo

--- react-other/src/App.tsx
+++ react-other/src/App.tsx
@@ -1,5 +1,30 @@
+import { create } from "zustand";
+
+type Store = {
+  count: number;
+  inc: () => void;
+};
+
+const useStore = create<Store>()((set) => ({
+  count: 1,
+  inc: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+function Counter() {
+  const { count, inc } = useStore();
+  return (
+    <div>
+      <button onClick={inc}>{count}</button>
+    </div>
+  );
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
