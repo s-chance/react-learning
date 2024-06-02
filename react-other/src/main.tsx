9785ca3b22c711ec2d52d5aeb34e1584ad99a0d8
s-chance <entropy.tree@outlook.com>
2024-06-02 13:18:45 +0800

    refactor(*): remove test code

--- react-other/src/main.tsx
+++ react-other/src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@/router";
-import { fetchChannelAPI } from "./apis/list";
+// import { fetchChannelAPI } from "./apis/list";
 
-fetchChannelAPI().then((res) => {
-  console.log(res.data.data.channels);
-});
+// fetchChannelAPI().then((res) => {
+//   console.log(res.data.data.channels);
+// });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider />
