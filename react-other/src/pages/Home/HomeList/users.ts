bf6d8a8366fa06ff6b5bf8c244d5f9cdca2f0157
s-chance <entropy.tree@outlook.com>
2024-06-02 14:43:29 +0800

    chore(*): mock data

---
+++ react-other/src/pages/Home/HomeList/users.ts
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    id: "1",
+    avatar:
+      "https://media.istockphoto.com/id/516449022/photo/lady-with-kayak.jpg?b=1&s=612x612&w=0&k=20&c=PsQQpGXVtV9dPDaH-DNEgeMAHjqkViCSQsSrkqlHKRk=",
+    name: "516449022",
+    description: "lady-with-kayak",
+  },
+  {
+    id: "2",
+    avatar:
+      "https://media.istockphoto.com/id/1383796215/photo/silhouette-of-man-holding-binoculars-on-mountain-peak-against-bright-sunlight-sky-background.jpg?b=1&s=612x612&w=0&k=20&c=7qil3OYFwP814_tFl1b-nPHloTptreuOeWA0HOLE5pE=",
+    name: "1383796215",
+    description:
+      "silhouette-of-man-holding-binoculars-on-mountain-peak-against-bright-sunlight-sky-background",
+  },
+  {
+    id: "3",
+    avatar:
+      "https://media.istockphoto.com/id/904172104/photo/weve-made-it-all-this-way-i-am-proud.jpg?b=1&s=612x612&w=0&k=20&c=JRmndTMyz627jxKiUjgXSJXrxwuAiHkHjefiRsyY2jc=",
+    name: "904172104",
+    description: "weve-made-it-all-this-way-i-am-proud",
+  },
+  {
+    id: "4",
+    avatar:
+      "https://media.istockphoto.com/id/1301431065/photo/man-hiker-on-top-of-a-mountain-peak.jpg?b=1&s=612x612&w=0&k=20&c=uK5SF008yXyqYqbcBC_wUz-SFzBmx3dGKmFiolLJ7oA=",
+    name: "1301431065",
+    description: "man-hiker-on-top-of-a-mountain-peak",
+  },
+];
